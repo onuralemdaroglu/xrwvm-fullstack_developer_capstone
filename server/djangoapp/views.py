9665d526d6c39293dbed624cbbe4acbe8bcee5e1
onuralemdaroglu <kronosscall@hotmail.com>
2024-03-28 11:09:29 -0400

    linting 4.2

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -66,7 +66,8 @@ def registration(request):
 
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name,
+        user = User.objects.create_user(
+            username=username, first_name=first_name,
             last_name=last_name, password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
@@ -132,10 +133,10 @@ def add_review(request):
     if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            # response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({"status": 401,
+                "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
-        
