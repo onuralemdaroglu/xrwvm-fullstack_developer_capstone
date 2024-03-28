9491be88e6340f55422aac9dffd440d5fc764f71
onuralemdaroglu <kronosscall@hotmail.com>
2024-03-28 11:21:57 -0400

    linting 4.3

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,8 +12,8 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'],
-                                                            description=data['description']))
+        car_make_instances.append(CarMake.objects.create(
+            name=data['name'], description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -51,4 +51,4 @@ def initiate():
 
     for data in car_model_data:
         CarModel.objects.create(name=data['name'], car_make=data['car_make'],
-                                    type=data['type'], year=data['year'])
+                                 type=data['type'], year=data['year'])
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,7 +7,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-# from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -87,8 +87,8 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, 
-                        "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -133,9 +133,9 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 def add_review(request):
     if (request.user.is_anonymous is False):
-        # data = json.loads(request.body)
+        data = json.loads(request.body)
         try:
-            # response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401,
